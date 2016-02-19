d2f507bc43153f13d4fa7730b8edf166e38f5efa
freedom <freedom_luo@sina.com>
2016-02-19 12:09:16 +0800

    完成“用户中心”的三个版块布局

--- Mai/Mai/UserCenter/Controller/UCUserCenterViewController.m
+++ Mai/Mai/UserCenter/Controller/UCUserCenterViewController.m
@@ -14,7 +14,11 @@
 
 @interface UCUserCenterViewController ()
 
-@property(nonatomic,strong) UIView *userView;//用户信息视图
+@property(nonatomic,strong) UIImageView *userView;//用户信息视图
+
+@property(nonatomic,strong) UIView *allOrdersView;//全部订单视图
+
+@property(nonatomic,strong) UIView *orderOperateView;//订单状态操作栏视图
 
 @end
 
@@ -29,6 +33,12 @@
     
     //初始化用户信息视图
     [self initUserView];
+    
+    //初始化全部订单视图
+    [self initAllOrdersView];
+    
+    //初始化订单状态操作栏视图
+    [self initOrderOperateView];
 }
 
 -(void)viewWillAppear:(BOOL)animated{
@@ -59,6 +69,40 @@
  */
 -(void)initUserView{
     //用户信息视图
+    self.userView=[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 180)];
+    self.userView.image=[UIImage imageNamed:@"user_center_userinfo_bg"];
+    self.userView.userInteractionEnabled=YES;
+    [self.view addSubview:self.userView];
+}
+
+/**
+ *  初始化全部订单视图
+ */
+-(void)initAllOrdersView{
+    //全部订单视图
+    self.allOrdersView=[[UIView alloc] initWithFrame:CGRectMake(0, self.userView.bottom, self.userView.width, 44)];
+    self.allOrdersView.backgroundColor=ThemeWhite;
+    [self.view addSubview:self.allOrdersView];
+    
+    //全部订单视图分割线
+    UIView *allOrdersLine=[[UIView alloc] initWithFrame:CGRectMake(0, self.allOrdersView.height-0.5, self.allOrdersView.width, 0.5)];
+    allOrdersLine.backgroundColor=UIColorFromRGB(0xdddddd);
+    [self.allOrdersView addSubview:allOrdersLine];
+}
+
+/**
+ *  初始化订单状态操作栏视图
+ */
+-(void)initOrderOperateView{
+    //订单状态操作栏视图
+    self.orderOperateView=[[UIView alloc] initWithFrame:CGRectMake(0, self.allOrdersView.bottom, self.allOrdersView.width, 60)];
+    self.orderOperateView.backgroundColor=ThemeWhite;
+    [self.view addSubview:self.orderOperateView];
+    
+    //订单状态操作栏视图分割线
+    UIView *orderOperateLine=[[UIView alloc] initWithFrame:CGRectMake(0, self.orderOperateView.height-1, self.orderOperateView.width, 1)];
+    orderOperateLine.backgroundColor=UIColorFromRGB(0xe6e6e6);
+    [self.orderOperateView addSubview:orderOperateLine];
 }
 
 #pragma mark 自定义方法
